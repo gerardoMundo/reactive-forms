3c7d389867de368dd71a302b67aa5c30d0a3060d
gerardoMundo <ger.mundo66@gmail.com>
2023-06-28 17:31:53 -0600

    switches form completed

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SwitchesPageComponent {
+  constructor(private fb: FormBuilder) {}
 
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', [Validators.required]],
+    notifications: [false],
+    terms: [false, Validators.requiredTrue],
+  });
+
+  //** Methods **
+  onSave(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+  }
+
+  //** Validators **
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Se necesitan mínimo ${errors['minlength'].requiredLength} caracteres`;
+      }
+    }
+    return null;
+  }
 }
